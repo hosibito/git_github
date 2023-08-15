972c6bdabd6c29bd80cf89ffbc185c6acf0ce127
FlowerCoder <hosibito@naver.com>
2023-08-15 14:32:33 +0900

    Modify ui.py to add le1, le2 and cb widget

--- ui.py
+++ ui.py
@@ -6,8 +6,11 @@ from PyQt5.QtWidgets import (
     QMessageBox,
     QPlainTextEdit,
     QHBoxLayout,
+    QLineEdit,
+    QComboBox,
 )
 from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore
 
 
 class View(QWidget):
@@ -16,35 +19,57 @@ class View(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.te1 = QPlainTextEdit()  # QPlainTextEdit 위젯 생성
-        self.te1.setReadOnly(True)  # 편집 불가능하도록 설정
+        # QPlainTextEdit 위젯 생성
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
 
-        self.btn1 = QPushButton(
-            "Message", self
-        )  # "Message"라는 텍스트를 가진 QPushButton 위젯 생성
-        self.btn2 = QPushButton("Clear", self)  # "Clear"라는 텍스트를 가진 QPushButton 위젯 생성
+        # QPushButton 위젯 생성
+        self.btn1 = QPushButton("Message", self)
+        self.btn2 = QPushButton("Clear", self)
 
-        hbox = QHBoxLayout()  # 수평 박스 레이아웃 생성
-        hbox.addStretch(1)  # 공간을 늘려서 버튼을 오른쪽으로 정렬
-        hbox.addWidget(self.btn1)  # 버튼1을 수평 박스에 추가
-        hbox.addWidget(self.btn2)  # 버튼2를 수평 박스에 추가
+        # QLineEdit 위젯 생성
+        self.le1 = QLineEdit("0", self)
+        self.le1.setAlignment(QtCore.Qt.AlignRight)
 
-        vbox = QVBoxLayout()  # 수직 박스 레이아웃 생성
-        vbox.addWidget(self.te1)  # QPlainTextEdit 위젯을 수직 박스에 추가
-        vbox.addLayout(hbox)  # 수평 박스 레이아웃을 수직 박스에 추가
-        vbox.addStretch(1)  # 공간을 늘려서 수직 박스를 아래로 정렬
+        self.le2 = QLineEdit("0", self)
+        self.le2.setAlignment(QtCore.Qt.AlignRight)
 
-        self.setLayout(vbox)  # 수직 박스 레이아웃을 윈도우의 레이아웃으로 설정
+        # QComboBox 위젯 생성
+        self.cb = QComboBox(self)
+        self.cb.addItems(["+", "-", "*", "/"])
 
-        self.setWindowTitle("Calculator")  # 윈도우 타이틀 설정
-        self.setWindowIcon(QIcon("icon.png"))  # 윈도우 아이콘 설정
-        self.resize(256, 256)  # 윈도우 크기 설정
-        self.show()  # 윈도우를 화면에 표시
+        # 수식 입력을 위한 QHBoxLayout 생성
+        hbox_formular = QHBoxLayout()
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
 
+        # 버튼을 담을 QHBoxLayout 생성
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+
+        # 전체 레이아웃 생성
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.te1)
+        vbox.addLayout(hbox_formular)
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+
+        # 전체 레이아웃을 윈도우에 설정
+        self.setLayout(vbox)
+
+        # 윈도우 타이틀과 아이콘 설정
+        self.setWindowTitle("Calculator")
+        self.setWindowIcon(QIcon("icon.png"))
+        self.resize(256, 256)
+        self.show()
+
+    # "Message" 버튼 클릭 시 호출되는 함수
     def activateMessage(self):
-        self.te1.appendPlainText(
-            "Button clicked!"
-        )  # QPlainTextEdit에 "Button clicked!" 텍스트 추가
+        self.te1.appendPlainText("Button clicked!")
 
+    # "Clear" 버튼 클릭 시 호출되는 함수
     def clearMessage(self):
-        self.te1.clear()  # QPlainTextEdit의 텍스트 모두 지우기
+        self.te1.clear()
