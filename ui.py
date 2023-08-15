e734eacdcece0d4d8ffe61906c7eed8e973e50be
FlowerCoder <hosibito@naver.com>
2023-08-15 14:52:53 +0900

    Modfy activateMessage function to add an argment

--- ui.py
+++ ui.py
@@ -19,32 +19,34 @@ class View(QWidget):
         self.initUI()
 
     def initUI(self):
-        # QPlainTextEdit 위젯 생성
+        # 텍스트 편집기 생성
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
 
-        # QPushButton 위젯 생성
+        # 메시지 버튼 생성
         self.btn1 = QPushButton("Message", self)
+        # 클리어 버튼 생성
         self.btn2 = QPushButton("Clear", self)
 
-        # QLineEdit 위젯 생성
+        # 첫 번째 입력 상자 생성
         self.le1 = QLineEdit("0", self)
         self.le1.setAlignment(QtCore.Qt.AlignRight)
 
+        # 두 번째 입력 상자 생성
         self.le2 = QLineEdit("0", self)
         self.le2.setAlignment(QtCore.Qt.AlignRight)
 
-        # QComboBox 위젯 생성
+        # 연산자 선택 상자 생성
         self.cb = QComboBox(self)
         self.cb.addItems(["+", "-", "*", "/"])
 
-        # 수식 입력을 위한 QHBoxLayout 생성
+        # 수식 입력 상자를 가로로 정렬하는 레이아웃 생성
         hbox_formular = QHBoxLayout()
         hbox_formular.addWidget(self.le1)
         hbox_formular.addWidget(self.cb)
         hbox_formular.addWidget(self.le2)
 
-        # 버튼을 담을 QHBoxLayout 생성
+        # 버튼을 가로로 정렬하는 레이아웃 생성
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(self.btn1)
@@ -60,16 +62,19 @@ class View(QWidget):
         # 전체 레이아웃을 윈도우에 설정
         self.setLayout(vbox)
 
-        # 윈도우 타이틀과 아이콘 설정
+        # 윈도우 타이틀 설정
         self.setWindowTitle("Calculator")
+        # 아이콘 설정
         self.setWindowIcon(QIcon("icon.png"))
+        # 윈도우 크기 설정
         self.resize(256, 256)
+        # 윈도우를 화면에 표시
         self.show()
 
-    # "Message" 버튼 클릭 시 호출되는 함수
-    def activateMessage(self):
-        self.te1.appendPlainText("Button clicked!")
+    # 메시지 활성화 함수
+    def activateMessage(self, text):
+        self.te1.appendPlainText(text)
 
-    # "Clear" 버튼 클릭 시 호출되는 함수
+    # 메시지 클리어 함수
     def clearMessage(self):
         self.te1.clear()
